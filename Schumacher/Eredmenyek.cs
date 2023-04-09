dda8a6a8917013d67b5f903bac9a00f79f0e001f
Ferenc Szilagyi <130311989+Latitude1977@users.noreply.github.com>
2023-04-09 21:26:33 +0200

    Add files via upload

---
+++ Schumacher/Eredmenyek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schumacher
+{
+    class Eredmenyek
+    {
+        private string datum, grandprix, team, status;
+        private int position, laps, points;
+
+        public Eredmenyek (string sor)
+        {
+            string[] d = sor.Split(';');
+            datum = d[0];
+            grandprix = d[1];
+            position = Convert.ToInt32(d[2]);
+            laps = Convert.ToInt32(d[3]);
+            points = Convert.ToInt32(d[4]);
+            team = d[5];
+            status = d[6];
+        }
+
+        public string Datum { get => datum; set => datum = value; }
+        public string Grandprix { get => grandprix; set => grandprix = value; }
+        public string Team { get => team; set => team = value; }
+        public string Status { get => status; set => status = value; }
+        public int Position { get => position; set => position = value; }
+        public int Laps { get => laps; set => laps = value; }
+        public int Points { get => points; set => points = value; }
+    }
+}
---
+++ Schumacher/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Schumacher
+{
+    
+    class Program
+    {
+        static List<Eredmenyek> lista = new List<Eredmenyek>();
+        //static List<string> hibaTipus = new List<string>();
+        static void beolvas()
+        {
+            foreach (var item in File.ReadAllLines("schumacher.csv", Encoding.UTF8).Skip(1))
+            {
+                Eredmenyek seged = new Eredmenyek(item);
+                lista.Add(seged);
+            }
+        }
+
+        static void feladat3()
+        {
+            Console.WriteLine($"3. feladat: Az állomány {lista.Count} sort tartalmaz.");
+
+        }
+
+        static void feladat4()
+        {
+            Console.WriteLine("4. feladat. A Magyar Nagydíj helyezései:");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].Grandprix.Split(' ')[0] == "Hungarian" && lista[i].Position > 0)
+                {
+                    Console.WriteLine($"\t{lista[i].Datum} : {lista[i].Position}. hely");
+                }
+            }
+        }
+
+        static void feladat5()
+        {
+            List<string> hibaOka = new List<string>();
+            
+            
+            Console.WriteLine("5. feladat: Hibastatisztika:");
+
+            foreach (Eredmenyek item in lista)
+            {
+                if (!hibaOka.Contains(item.Status))
+                { 
+                    hibaOka.Add(item.Status); 
+                }
+            }
+            int[] hibaDB = new int[hibaOka.Count];
+            foreach (Eredmenyek item in lista)
+            {
+                for (int i = 0; i < hibaOka.Count; i++)
+                {
+                    if(item.Status == hibaOka[i])
+                    {
+                        hibaDB[i]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < hibaOka.Count; i++)
+            {
+                if(hibaOka[i] != "Finished" && hibaDB[i] > 2)
+                { 
+                Console.WriteLine("\t{0} : {1} db", hibaOka[i], hibaDB[i]);
+                }
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            beolvas();
+            feladat3();
+            feladat4();
+            feladat5();
+            
+            Console.ReadKey();
+        }
+    }
+}
